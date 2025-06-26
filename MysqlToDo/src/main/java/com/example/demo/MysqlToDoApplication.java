51327300159b722ee0eb9ea5534f56d1413cd483
itf <itf@host.docker.internal>
2025-06-26 11:55:29 +0900

    first commit

---
+++ MysqlToDo/src/main/java/com/example/demo/MysqlToDoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlToDoApplication.class, args);
+	}
+
+}
